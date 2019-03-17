7d0d23d8d315dbaf09e3b3aa427aadd406bf300d
ntp945812 <ntp945812@hotmail.com>
2019-03-17 08:58:13 +0800

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by 謝冠緯 on 2019/3/17.
+//  Copyright © 2019年 謝冠緯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
